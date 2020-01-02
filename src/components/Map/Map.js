0d878a764a0df7b72f1b8353153db05ed63a31c7
unknown <nirmalkrishnav@gmail.com>
2020-01-02 22:49:33 +0530

    added first fetch

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,5 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 function Map() {
+
+    useEffect(() => {
+        fetchItems();
+    }, []);
+
+    const fetchItems = async () => {
+        const data = await fetch('http://localhost:5000/api/v1/stores');
+        console.log(data);
+    }
+
     return (
         <h1>Map</h1>
     );
